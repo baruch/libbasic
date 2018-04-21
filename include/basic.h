9b5a36d3d0194d49b5021a34cf9adc3b5a198a21
Baruch Even <baruch@ev-en.org>
2018-04-21 23:24:34 +0300

    Add ifndef guards

--- include/basic.h
+++ include/basic.h
@@ -1,21 +1,37 @@
 #ifndef LIBBASIC_H
 #define LIBBASIC_H
 
+#ifndef STR
 #define __tostr(x) #x
 #define STR(x) __tostr(x)
+#endif
 
+#ifndef likely
 #define likely(expr)    __builtin_expect((expr) != 0, 1)
+#endif
+
+#ifndef unlikely
 #define unlikely(expr)  __builtin_expect((expr) != 0, 0)
+#endif
 
+#ifndef ARRAY_SIZE
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#endif
 
+#ifndef MIN
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
-#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#endif
 
+#ifndef MAX
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#endif
+
+#ifndef UNUSED
 #ifdef __GNUC
 #define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
 #else
 #define UNUSED(x) UNUSED_ ## x
+#endif
 #endif
 
 int sock_set_nonblock(int sock);
